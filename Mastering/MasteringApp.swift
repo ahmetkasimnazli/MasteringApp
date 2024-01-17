14bec3f824cf82fd7780fab65ea6c18cbddf5b57
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-01-17 16:44:26 +0200

    Initial Commit

---
+++ Mastering/MasteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringApp.swift
+//  Mastering
+//
+//  Created by Ahmet Kasım Nazlı on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MasteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
