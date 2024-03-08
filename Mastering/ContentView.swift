b89b466804e5320eab6583960c96224f82fed593
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-03-08 23:37:59 +0300

    service class updates

--- Mastering/ContentView.swift
+++ Mastering/ContentView.swift
@@ -8,17 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = DolbyIOViewModel()
+    @State private var isImporting = false
+    @State var selectedFileName: String = ""
+    @State var fileURL: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Pick") {
+                    Button {
+                        isImporting = true
+                    } label: {
+                        Label(selectedFileName, systemImage: "folder.fill.badge.plus")
+                    }
+                }
+                Section("Upload") {
+                    Button {
+                        print("Upload Button tapped")
+                        Task {
+                            viewModel.uploadMediaInput()
+                            viewModel.uploadMedia(localFilePath: fileURL)
+                        }
+                    } label: {
+                        Label("Upload", systemImage: "square.and.arrow.up")
+                    }
+                    
+                    Button {
+                        print("Enhance Button tapped")
+                        Task {
+                            viewModel.enhanceMedia()
+                        }
+                    } label: {
+                        Label("Start Enhancing", systemImage: "sparkles")
+                    }
+                }
+                Section("Save") {
+                    Button {
+                        print("Status Button tapped")
+                        Task {
+                            viewModel.getEnhanceJobStatus()
+                        }
+                    } label: {
+                        Label("Get Enhance Job Status", systemImage: "questionmark.circle")
+                    }
+                }
+                
+                Button {
+                    print("Save Button tapped")
+                    Task {
+                        viewModel.downloadEnhancedMedia()
+                    }
+                } label: {
+                    Label("Save Enhanced Media File", systemImage: "square.and.arrow.down")
+                }
+            }
+        .fileImporter(isPresented: $isImporting, allowedContentTypes: [.audio]) { result in
+            do {
+                let file = try result.get()
+                selectedFileName = file.lastPathComponent
+                fileURL = String(file.path)
+                print("File URL: \(fileURL)")
+                 
+            } catch {
+                
+            }
         }
-        .padding()
+        .navigationTitle("DolbyIO")
+    }
+    .onAppear {
+        viewModel.getToken()
+        
     }
 }
-
-#Preview {
-    ContentView()
 }
--- Mastering/ViewModel.swift
+++ Mastering/ViewModel.swift
@@ -6,66 +6,127 @@
 //
 
 import Foundation
-import Alamofire
 
-class DolbyMediaViewModel {
-    private let apiKey: String
+
+@MainActor
+class DolbyIOViewModel: ObservableObject {
     
-    init(apiKey: String) {
-        self.apiKey = apiKey
+    private let service = DolbyIOService.shared
+    @Published var token: String?
+    @Published var uploadLink: String?
+    @Published var uploadResponse: HTTPURLResponse?
+    @Published var jobID: String?
+    @Published var error: Error?
+    
+    func getToken() {
+        Task {
+            do {
+                let receivedToken = try await service.getToken()
+                self.token = receivedToken
+            } catch {
+                self.error = error
+            }
+        }
     }
     
-    func uploadMediaFile(filePath: String, completion: @escaping (Result<Void, Error>) -> Void) {
-        let headers: HTTPHeaders = [
-            "Authorization": "Bearer \(apiKey)",
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        ]
+    func uploadMediaInput() {
+        guard let token = self.token else {
+            self.error = NSError(domain: "TokenNotAvailable", code: 0, userInfo: nil)
+            return
+        }
         
-        let parameters: [String: Any] = [
-            "url": "dlb://in/example.wav"
-        ]
+        Task {
+            do {
+                let receivedLink = try await service.uploadMediaInput(token: token)
+                self.uploadLink = receivedLink
+            } catch {
+                self.error = error
+            }
+        }
+    }
+    
+    func uploadMedia(localFilePath: String) {
+        guard let uploadLink = self.uploadLink else {
+            self.error = NSError(domain: "UploadLinkNotAvailable", code: 0, userInfo: nil)
+            return
+        }
         
-        AF.request("https://api.dolby.com/media/input", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
-            .validate(statusCode: 200..<300)
-            .responseDecodable(of: Model.self) { (response) in
-                switch response.result {
-                case .success(let decodedResponse):
-                    let url = decodedResponse.url // Assuming your response structure has a 'url' property
-                    
-                    print("Upload \(filePath) to \(url)")
-                    let fileURL = URL(fileURLWithPath: filePath)
-                    
-                    AF.upload(fileURL, to: url, method: .put, headers: headers)
-                        .uploadProgress { progress in
-                            print("Upload Progress: \(progress.fractionCompleted)")
-                        }
-                        .response { response in
-                            switch response.result {
-                            case .success:
-                                completion(.success(()))
-                            case .failure(let error):
-                                completion(.failure(error))
-                            }
-                        }
-                    
-                case .failure(let error):
-                    completion(.failure(error))
+        Task {
+            do {
+                let httpResponse = try await service.uploadMedia(localFilePath: localFilePath, preSignedURL: uploadLink)
+                
+                self.uploadResponse = httpResponse
+                print("DEBUG: filePath: \(localFilePath) uploadLink:  \(uploadLink)")
+            } catch {
+                self.error = error
+            }
+        }
+    }
+    
+    func enhanceMedia() {
+        guard let token = self.token else {
+            self.error = NSError(domain: "TokenNotAvailable", code: 0, userInfo: nil)
+            return
+        }
+            Task {
+                do {
+                    let result = try await service.enhanceMedia(apiToken: token)
+                    self.jobID = result
+                    print("JOB ID: \(result)")
+                    self.error = nil
+                } catch {
+                    self.error = error
+                    print("DEBUG Error: \(error.localizedDescription)")
                 }
             }
+        }
+    
+    func getEnhanceJobStatus() {
+        guard let token = self.token else {
+            self.error = NSError(domain: "TokenNotAvailable", code: 0, userInfo: nil)
+            return
+        }
+        guard let jobID = self.jobID else {
+            self.error = NSError(domain: "TokenNotAvailable", code: 0, userInfo: nil)
+            return
+        }
+
+        Task {
+            do {
+                let result = try await service.getEnhanceJobStatus(apiToken: token, jobID: jobID)
+                print("Status: \(result)")
+                self.error = nil
+            } catch {
+                self.error = error
+                print("DEBUG Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func downloadEnhancedMedia() {
+        guard let token = self.token else {
+            self.error = NSError(domain: "TokenNotAvailable", code: 0, userInfo: nil)
+            return
+        }
+
+        Task {
+            do {
+                let result = try await service.downloadEnhancedMedia(apiToken: token)
+                print("Successfull")
+                print("Result: \(result)")
+                self.error = nil
+            } catch {
+                self.error = error
+                print("DEBUG Error: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
-// Kullanım örneği:
-let apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9...." // API anahtarını buraya ekleyin
-let viewModel = DolbyMediaViewModel(apiKey: apiKey)
-let filePath = "path/to/your/file.wav" // Yüklemek istediğiniz dosyanın yolunu buraya ekleyin
 
-viewModel.uploadMediaFile(filePath: filePath) { result in
-    switch result {
-    case .success:
-        print("File uploaded successfully.")
-    case .failure(let error):
-        print("Error uploading file: \(error)")
-    }
-}
+
+
+
+
+
+
