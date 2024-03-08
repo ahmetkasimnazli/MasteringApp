9164e95f14c77f68fb7f4ddfcecf6a6b12f0817a
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-03-08 23:34:06 +0300

    service class updates

---
+++ Mastering/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  Mastering
+//
+//  Created by Ahmet Kasım Nazlı on 17.01.2024.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
