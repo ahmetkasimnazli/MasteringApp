3b20d114bc3db3f64b6978d71ceb92a773413448
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-03-26 18:48:34 +0300

    Update UI adjustments. howTo? sheet added. logic adjustments

---
+++ Mastering/Views/HowToMasterView.swift
@@ -0,0 +1,18 @@
+//
+//  HowToMasterView.swift
+//  Mastering
+//
+//  Created by Ahmet Kasım Nazlı on 26.03.2024.
+//
+
+import SwiftUI
+
+struct HowToMasterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HowToMasterView()
+}
