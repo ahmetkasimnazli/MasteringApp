761180eda9ac9e23b635aaa5c4d6ac572f820182
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-03-27 22:22:29 +0300

    UPDATE UI updates. UX updates. Bug fixes. ResultAndSaveView added.

---
+++ Mastering/Views/AppPrimaryButton.swift
@@ -0,0 +1,8 @@
+//
+//  AppPrimaryButton.swift
+//  Mastering
+//
+//  Created by Ahmet Kasım Nazlı on 27.03.2024.
+//
+
+import Foundation
---
+++ Mastering/Views/ResultAndSaveView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultAndSaveView.swift
+//  Mastering
+//
+//  Created by Ahmet Kasım Nazlı on 27.03.2024.
+//
+
+import SwiftUI
+
+struct ResultAndSaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultAndSaveView()
+}
